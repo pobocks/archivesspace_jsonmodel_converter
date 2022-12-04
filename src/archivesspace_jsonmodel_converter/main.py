e49702f516ffc6c9e81e8e6ecc2be7bceacdae22
Dave Mayo <pobocks@gmail.com>
2022-12-04 15:29:11 -0500

    Added logging

--- src/archivesspace_jsonmodel_converter/main.py
+++ src/archivesspace_jsonmodel_converter/main.py
@@ -1,5 +1,7 @@
 import click
-
+from .logger import get_logger
 @click.command()
 def main():
+    log = get_logger('main')
+    log.info("Running main entry point", extra_var="added some stuff")
     print("Hello, world!")
