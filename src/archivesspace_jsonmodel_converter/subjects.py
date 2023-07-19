78db383e5b4457d8a5afb7083122887414246307
Bobbi Fox <bobbi@bobbifox.net>
2023-07-19 18:54:15 -0400

    account for duplicates. improve messages

--- src/archivesspace_jsonmodel_converter/subjects.py
+++ src/archivesspace_jsonmodel_converter/subjects.py
@@ -17,16 +17,21 @@ xw = None
 conn = None # postgres connection
 log = None
 
-def add_to_aspace(orig_id, subject, aid):
+def add_to_aspace(tablename, orig_id, subject, aid):
     ''' Add/update a subject to ArchivesSpace'''
     aspace_id = None
     response = None
+    subj = None
     if aid is not None:
         try:
             subj = client.get(aid).json()
             subject['lock_version'] = subj['lock_version']
+            for inx, term in enumerate(subj['terms']):
+                if inx < len(subject['terms']) and term['term'] == subject['terms'][inx]['term']:
+                    subject['terms'][inx]['lock_version'] = term['lock_version']
         except Exception as e:
-            log.error(f'unable to correctly retrieve lock_version for uri {aid}')                
+            log.error(f'unable to correctly retrieve lock_version for uri {aid}') 
+            return None               
         response = client.post(aid, json=subject).json()
     else:
         response = client.post('subjects', json=subject).json()
@@ -34,11 +39,16 @@ def add_to_aspace(orig_id, subject, aid):
         aspace_id = response['uri']
     elif 'error' in response:
         err = response['error']
+         # treat a conflicting record as a win  (i.e., it's a duplicate in the original db)
+        if 'conflicting_record' in err:
+            aspace_id = err['conflicting_record'][0]
+            log.warn(f'Possible duplicate for ID {orig_id} in {tablename} found with URI of {aspace_id}')
+        else:
         # look for a reason for the error
-        error = err
-        if 'source' in err:
-            error = err['source'][0]
-        log.error(f"Error detected for ID {orig_id}",error=error)
+            error = err
+            if 'source' in err:
+                error = err['source'][0]
+            log.error(f"Error detected for ID {orig_id} in {tablename}",error=error)
     else:
         log.error(f"Item {orig_id} not created for unknown reasons", error=response)
     return aspace_id
@@ -91,19 +101,19 @@ def process_subjects(tablename, firstfield, source):
             orig_id = row[0]
             orig_val = row[1]
             if orig_val is None:
-                log.warn(f"Original ID {orig_id} has 'None' as a value!")
+                log.warn(f"Original ID {orig_id} in {tablename} has 'None' as a value!")
                 break
             try:
                 subject = create_subject_json(orig_val, firstfield, source)
                 aid = xw.get_aspace_id(tablename, orig_id)
-                aspace_id = add_to_aspace(orig_id, subject, aid)
+                aspace_id = add_to_aspace(tablename, orig_id, subject, aid)
                 if aspace_id is not None:
                     added = xw.add_or_update(tablename, orig_id,orig_val,aspace_id)
                     if added:
                         ct = ct + 1
                 #TODO: what do we do with None aspace_ids?
                 else:
-                    log.warn(f"{orig_id} ({orig_val}) was not converted")
+                    log.warn(f"{orig_id} in {tablename} ({orig_val}) was not converted")
             except Exception as e:
                 traceback.print_exc(e)
                 log.error(f"Exception  triggered on {orig_id} ({orig_val}), which will not be converted", error=e)
